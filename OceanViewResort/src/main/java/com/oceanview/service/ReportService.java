cd42bc64595f79289a4ceaff7f704d5b0b57193b
sadeepasewmini <sewminisadeepa102@gmail.com>
2026-02-21 22:40:53 +0530

    report service commit

--- OceanViewResort/src/main/java/com/oceanview/service/ReportService.java
+++ OceanViewResort/src/main/java/com/oceanview/service/ReportService.java
@@ -15,10 +15,7 @@ public class ReportService {
         Map<String, Double> reportData = new HashMap<>();
         String sql = "SELECT room_type, SUM(total_bill) as revenue FROM reservations GROUP BY room_type";
         
-        try (Connection conn = DatabaseConfig.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
-            
+       
             while (rs.next()) {
                 reportData.put(rs.getString("room_type"), rs.getDouble("revenue"));
             }
